a5758b19526fcc1f3e8f7f3450ddf7e1c671a044
Shiva <dasshivashish11@gmail.com>
2024-06-11 04:06:04 +0530

    add citation

--- elf/seccomp.c
+++ elf/seccomp.c
@@ -26,43 +26,27 @@ void handler(int sig, siginfo_t *info, void *arg) {
   syscall(SYS_exit, 0);
 }
 
+/* The below function install_filter() and the macro ARRAY_SIZE is a modified version 
+ * of the source code available at https://man7.org/linux/man-pages/man2/seccomp.2.html 
+ * in the "Examples" section */
+
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-static int install_filter(unsigned int t_arch) {
-  unsigned int upper_nr_limit = 0xffffffff;
-
+static int install_filter() {
+  // To keep the filter small, we do not check the architecture because it is known at
+  // compile time which arhitecture we are compiling for
+  
+  // To add new system calls that need to be allowed, add them at the top of the first
+  // BPF_JUMP statement and with a number one more than the first statement's 'jt' field
   struct sock_filter filter[] = {
-      /* [0] Load architecture from 'seccomp_data' buffer into
-             accumulator. */
-      BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, arch))),
-
-      /* [1] Jump forward 5 instructions if architecture does not
-             match 't_arch'. */
-      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, t_arch, 0, 4),
-
-      /* [2] Load system call number from 'seccomp_data' buffer into
-             accumulator. */
       BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, nr))),
-
-      /* [4] Jump forward 1 instruction if system call number
-             does not match 'syscall_nr'. */
       BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_write, 4, 0),
       BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_mprotect, 3, 0),
       BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_rt_sigreturn, 2, 0),
       BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_exit, 1, 0),
       BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_read, 0, 1),
- 
-
-      /* [5] Matching architecture and system call: don't execute
-         the system call, and return 'f_errno' in 'errno'. */
       BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
-
-      /* [6] Destination of system call number mismatch: allow other
-             system calls. */
       BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
-
-      /* [7] Destination of architecture mismatch: kill process. */
-      BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL_PROCESS),
   };
 
   struct sock_fprog prog = {
@@ -78,17 +62,14 @@ static int install_filter(unsigned int t_arch) {
   return 0;
 }
 
+
 int main() {
   struct sigaction sa;
   sa.sa_sigaction = handler;
   sa.sa_flags = SA_SIGINFO;
   sigaction(SIGSYS, &sa, NULL);
   prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
-#ifdef __aarch64__ 
-  install_filter(AUDIT_ARCH_AARCH64);
-#else
-  install_filter(AUDIT_ARCH_x86_64);
-#endif
+  install_filter();
   kill(9, getpid());
   syscall(SYS_exit, 0);
   return 0;
