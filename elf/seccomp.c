941e87d9bc0dfaa9adea6c560495588f075a1bc2
Shiva <dasshivashish11@gmail.com>
2024-06-11 03:49:29 +0530

    removed library dependency

--- elf/seccomp.c
+++ elf/seccomp.c
@@ -1,54 +1,95 @@
-#include <seccomp.h>
+#include <signal.h>
 #include <stddef.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <sys/mman.h>
 #include <syscall.h>
+#include <unistd.h>
+#include <sys/prctl.h>
+#include <linux/audit.h>
+#include <linux/filter.h>
+#include <linux/seccomp.h>
+#include <sys/stat.h>
 
-int seccomp_start() {
-	int rc = -1;
-        scmp_filter_ctx ctx;
-        ctx = seccomp_init(SCMP_ACT_TRAP);
-        if (ctx == NULL)
-             goto out;
-        rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
-	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
-        rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
-	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(mprotect), 0);
-	rc = seccomp_rule_add(ctx, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0);
-        rc = seccomp_load(ctx);
-        if (rc < 0)
-           goto out;
-	return 0;
-
-out:
-        seccomp_release(ctx);
-        return -rc;
+void handler(int sig, siginfo_t *info, void *arg) {
+  write(1, "Hello World\n", 13);
+#ifdef __x86_64_
+  uint64_t addr = ((uint64_t)info->si_call_addr) - 2;
+  uint64_t maddr = (uint64_t)addr - (addr % 4096);
+  mprotect((void *)maddr, 4096, PROT_WRITE | PROT_READ | PROT_EXEC);
+  unsigned char *ptr = (void *)addr;
+  *(ptr) = 0x90;
+  *(ptr + 1) = 0xCC;
+  mprotect((void *)maddr, 4096, PROT_READ | PROT_EXEC);
+  void (*ret)() = ptr + 2;
+  ret();
+#endif
+  syscall(SYS_exit, 0);
 }
 
-void handler(int sig, siginfo_t* info, void* arg) {
-	write(1, "Hello World\n", 13);
-	uint64_t addr = ((uint64_t)info->si_call_addr) - 2;
-	uint64_t maddr = (uint64_t) addr - (addr % 4096);
-	mprotect((void*) maddr, 4096, PROT_WRITE | PROT_READ | PROT_EXEC);
-	unsigned char* ptr = (void*)addr;
-	*(ptr) = 0x90;
-	*(ptr + 1) = 0xCC;
-	mprotect((void*)maddr, 4096, PROT_READ | PROT_EXEC);	
-	void (*ret)() = ptr + 2;
-	ret();
-}
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
+static int install_filter(unsigned int t_arch) {
+  unsigned int upper_nr_limit = 0xffffffff;
+
+  struct sock_filter filter[] = {
+      /* [0] Load architecture from 'seccomp_data' buffer into
+             accumulator. */
+      BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, arch))),
+
+      /* [1] Jump forward 5 instructions if architecture does not
+             match 't_arch'. */
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, t_arch, 0, 4),
+
+      /* [2] Load system call number from 'seccomp_data' buffer into
+             accumulator. */
+      BPF_STMT(BPF_LD | BPF_W | BPF_ABS, (offsetof(struct seccomp_data, nr))),
+
+      /* [4] Jump forward 1 instruction if system call number
+             does not match 'syscall_nr'. */
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_write, 4, 0),
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_mprotect, 3, 0),
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_rt_sigreturn, 2, 0),
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_exit, 1, 0),
+      BPF_JUMP(BPF_JMP | BPF_JEQ | BPF_K, SYS_read, 0, 1),
+ 
+
+      /* [5] Matching architecture and system call: don't execute
+         the system call, and return 'f_errno' in 'errno'. */
+      BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_ALLOW),
+
+      /* [6] Destination of system call number mismatch: allow other
+             system calls. */
+      BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_TRAP),
+
+      /* [7] Destination of architecture mismatch: kill process. */
+      BPF_STMT(BPF_RET | BPF_K, SECCOMP_RET_KILL_PROCESS),
+  };
+
+  struct sock_fprog prog = {
+      .len = ARRAY_SIZE(filter),
+      .filter = filter,
+  };
+
+  if (syscall(SYS_seccomp, SECCOMP_SET_MODE_FILTER, 0, &prog)) {
+    perror("seccomp");
+    return 1;
+  }
+
+  return 0;
+}
 
 int main() {
-	struct sigaction sa;
-	sa.sa_sigaction = handler;
-	sa.sa_flags = SA_SIGINFO;
-	sigaction(SIGSYS, &sa, NULL);
-	seccomp_start();
-	kill(1, getpid());
-	syscall(SYS_exit, 0);
-	return 0;
+  struct sigaction sa;
+  sa.sa_sigaction = handler;
+  sa.sa_flags = SA_SIGINFO;
+  sigaction(SIGSYS, &sa, NULL);
+  prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0);
+#ifdef __aarch64__ 
+  install_filter(AUDIT_ARCH_AARCH64);
+#else
+  install_filter(AUDIT_ARCH_x86_64);
+#endif
+  kill(9, getpid());
+  syscall(SYS_exit, 0);
+  return 0;
 }
